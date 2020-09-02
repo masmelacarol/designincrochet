30b367112855867166715c8004d54ee7e60bf781
masmelacarol <carolstefannym@gmail.com>
2020-09-01 21:54:12 -0500

    [upd]: login, register component / styles

--- src/app/authentication/components/register/register.component.ts
+++ src/app/authentication/components/register/register.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
   }
 
+  ngOnInit(): void {}
+
+  register(event: Event) {
+    event.preventDefault();
+    console.log('hola');
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(40)]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
 }
