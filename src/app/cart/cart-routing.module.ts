5b033b3f07ef688a129053786ee86cabab3bfad3
masmelacarol <carolstefannym@gmail.com>
2020-09-02 12:44:15 -0500

    [upd]: cart component

--- src/app/cart/cart-routing.module.ts
+++ src/app/cart/cart-routing.module.ts
@@ -4,7 +4,7 @@ import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
   {
-    path: '/',
+    path: '',
     component: CartComponent,
   },
 ];
--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { MaterialDesignModule } from '@material-design/material-design.module';
 import { CartRoutingModule } from './cart-routing.module';
 import { CartComponent } from './components/cart/cart.component';
 
-
 @NgModule({
   declarations: [CartComponent],
-  imports: [
-    CommonModule,
-    CartRoutingModule
-  ]
+  imports: [CommonModule, CartRoutingModule, MaterialDesignModule],
 })
-export class CartModule { }
+export class CartModule {}
