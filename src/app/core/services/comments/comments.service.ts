854d79abf83233f4203d0de3c5e86652f83791b0
masmelacarol <carolstefannym@gmail.com>
2020-09-04 17:00:41 -0500

    [upd]: addComment

--- src/app/core/services/comments/comments.service.ts
+++ src/app/core/services/comments/comments.service.ts
@@ -1,20 +1,35 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Comments } from '@core/models/model';
 import { environment } from '@environments/environment';
-import { Observable, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommentsService {
+  private comments = new BehaviorSubject<[]>([]);
+  comments$ = this.comments.asObservable();
+
   constructor(private http: HttpClient) {}
 
-  getAllComentsByProduct(idProduct): Observable<any> {
-    return this.http.get(`${environment.url_api}/comments/${idProduct}`).pipe(
-      catchError((error) => this.handleError(error)),
-      map((response: any) => response.body)
-    );
+  getAllComentsByProduct(idProduct): Observable<void> {
+    return this.http
+      .get<Comments>(`${environment.url_api}/comments/${idProduct}`)
+      .pipe(
+        catchError((error) => this.handleError(error)),
+        map((response: any) => this.comments.next(response.body))
+      );
+  }
+
+  addComment(idUser, idProduct, comment, rating): Observable<Comments> {
+    return this.http.post<Comments>(`${environment.url_api}/comments/`, {
+      users: idUser,
+      products: idProduct,
+      comment,
+      rating,
+    });
   }
 
   private handleError(error: HttpErrorResponse): Observable<never> {
--- src/app/products/components/comments/comments.component.ts
+++ src/app/products/components/comments/comments.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Comments } from '@core/models/model';
+import { AuthService } from '@core/services/auth/auth.service';
 import { CommentsService } from '@core/services/comments/comments.service';
 import { RatingChangeEvent } from 'angular-star-rating';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-comments',
@@ -10,18 +13,18 @@ import { RatingChangeEvent } from 'angular-star-rating';
   styleUrls: ['./comments.component.scss'],
 })
 export class CommentsComponent implements OnInit {
-  productId;
-  email = 'carolstefannym@gmail.com';
+  private productId: string;
+  comments$: Observable<Comments[]>;
   score = 0;
   form: FormGroup;
-  comments;
 
   fallbacks = ['monsterid', 'retro', 'robohash', 'wavatar'];
 
   constructor(
     private formBuilder: FormBuilder,
     private commentsService: CommentsService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private authService: AuthService
   ) {
     this.buildForm();
   }
@@ -31,9 +34,8 @@ export class CommentsComponent implements OnInit {
       this.productId = params.id;
       this.commentsService
         .getAllComentsByProduct(this.productId)
-        .subscribe((comments) => {
-          console.log('CommentsComponent -> comments', comments);
-          this.comments = comments;
+        .subscribe(() => {
+          this.comments$ = this.commentsService.comments$;
         });
     });
   }
@@ -44,12 +46,24 @@ export class CommentsComponent implements OnInit {
 
   addComment(event: Event): void {
     event.preventDefault();
-    console.log('hola');
+    if (this.form.valid && this.score > 0) {
+      const value = this.form.value;
+      this.authService.hasUser().subscribe((user) => {
+        this.authService.getUser(user.uid).subscribe((custom) => {
+          this.commentsService
+            .addComment(custom._id, this.productId, value.comment, this.score)
+            .subscribe(() => {
+              this.score = 0;
+              this.form.reset();
+            });
+        });
+      });
+    }
   }
 
   private buildForm(): void {
     this.form = this.formBuilder.group({
-      rating: [0, [Validators.required]],
+      rating: [0, [Validators.required, Validators.min(0), Validators.max(5)]],
       comment: ['', [Validators.required, Validators.minLength(20)]],
     });
   }
